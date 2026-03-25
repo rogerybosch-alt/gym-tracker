e87d4ce2bbd9c0480a03d15ec84b26296c7c97e2
rogerybosch-alt <rogerybosch@gmail.com>
2026-03-25 17:50:44 +0100

    modify3

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,28 @@
-const CACHE = 'gym-tracker-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/static/js/main.chunk.js',
-  '/static/js/bundle.js',
-  '/static/js/vendors~main.chunk.js',
-];
+const CACHE = 'gym-tracker-v2';
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      return caches.open(CACHE).then(c => {
-        c.put(e.request, res.clone());
-        return res;
-      });
-    }))
+    caches.open(CACHE).then(cache =>
+      cache.match(e.request).then(cached => {
+        const fetchPromise = fetch(e.request).then(res => {
+          cache.put(e.request, res.clone());
+          return res;
+        }).catch(() => cached);
+        return cached || fetchPromise;
+      })
+    )
   );
 });
