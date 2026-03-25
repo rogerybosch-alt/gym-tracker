abf3aaf8cb8b3c0dcf6d761110924e158e722919
rogerybosch-alt <rogerybosch@gmail.com>
2026-03-25 17:40:46 +0100

    modify2

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'gym-tracker-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/static/js/main.chunk.js',
+  '/static/js/bundle.js',
+  '/static/js/vendors~main.chunk.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      return caches.open(CACHE).then(c => {
+        c.put(e.request, res.clone());
+        return res;
+      });
+    }))
+  );
+});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -11,3 +10,9 @@ root.render(
     <App />
   </StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
